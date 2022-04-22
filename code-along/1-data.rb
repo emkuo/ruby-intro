ec75316341fe83eb493dc2a0874c83edcc1f9c44
emkuo <emily.kuo@kellogg.northwestern.edu>
2022-04-22 19:26:53 +0000

    class_updates

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -2,18 +2,31 @@
 # is the same as where this file is located and then run:
 # ruby 1-data.rb
 
+#puts "Hello, world"
+
 # In Ruby, there are different *types* of data:
 
 # Numbers
+# puts 7
+# puts "tacos"
 
 # Perform simple math with numbers
+# puts 5+2 
 
 # Strings
 
 # Combine strings together
+# puts "#{3+3+3} tacos"
 
 # Variables
+quantity = 3 + 3 + 3 
+food = "tacos"
 
 # Combine strings and variables
+# puts "#{quantity} #{food}"
+
 
 # String manipulation
+# puts "Hello" .upcase
+# puts "Hello" .reverse
+puts "Hello" .swapcase 
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,60 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+#this_is_true = true 
+#puts this_is_true
+#this_is_false = false 
+#puts this_is_false
 
-# Boolean Expressions
+# Boolean Expressions - always going to give you either true or false as an output 
+# puts 3 == 2     # must have the double = sign in a Boolean expression
+# puts 3 != 2     # 
+# puts 3 > 2
+# puts 3 < 2     
 
 # If Conditional Logic
+# if 3 > 2 
+#     # do something / indentation doesn't matter 
+#     puts "yep, 3 is greater than 2" 
+# end
+
+user_entered_password = "tacos"
+real_password = "secret"
+
+if user_entered_password == real_password
+    puts "You're in!"
+else 
+    puts "nope."
+end
 
 # If/Else Conditional Logic
 
+# if user_entered_password == real_password
+#     puts "You're in!"
+# else 
+#     puts "nope."
+# end
+
+
 # Elsif Conditional Logic
+# Sometimes, there are more than one condition that one or the other, so you can put other outcomes using elseif 
+
+# your_team_score = 3 
+# other_team_score = 3 
+
+# if your_team_score > other_team_score
+#     puts "You win!"
+# elsif your_team_score == other_team_score
+#     puts "You tied."
+# else
+#     puts "You lose!"
+# end 
 
 # Combining Expressions
+
+temp = 68 
+precip = 0 
+
+if (temp >= 65 && temp <= 90) || (precip == 0) 
+    puts "it's nice outside!"
+end 
